86db68d671f58cb8e87d4521e6b343235f52c788
dityudhist <105841114122@student.unismuh.ac.id>
2025-07-14 01:25:02 +0800

    tugas2

--- app/index.tsx
+++ app/index.tsx
@@ -77,8 +77,8 @@ export default function App() {
   const handleImagePress = (id: number) => {
     setImages(prevImages =>
       prevImages.map(img => {
-        if (img.id === id && img.scale < 2.4) {
-          const nextScale = Math.min(img.scale + 1.2, 2.4);
+        if (img.id === id && img.scale < 2.0) {
+          const nextScale = Math.min(img.scale * 1.2, 2.0);
           return {
             ...img,
             scale: nextScale,
@@ -109,7 +109,7 @@ export default function App() {
           <TouchableOpacity
             key={image.id}
             onPress={() => handleImagePress(image.id)}
-            disabled={image.scale >= 2.4}
+            disabled={image.scale >= 2.0}
             style={styles.gridCell}
           >
             <Image
