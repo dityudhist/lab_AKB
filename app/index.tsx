e9ce2f9760bbb4efe3d692130c3bb69efd6b903e
dityudhist <105841114122@student.unismuh.ac.id>
2025-07-14 19:14:01 +0800

    tugas

--- app/index.tsx
+++ app/index.tsx
@@ -63,48 +63,48 @@ const ImageCell: React.FC<ImageCellProps> = ({ primaryUrl, alternateUrl }) => {
 const imageData = [
   {
     id: '1',
-    primary: 'https://i.pinimg.com/736x/5b/be/35/5bbe354ee3c7c044039dbafe876ffb39.jpg',
-    alternate: 'https://i.pinimg.com/736x/98/05/d1/9805d186ebd59bfad0853c652a43b00a.jpg',
+    primary: 'https://picsum.photos/id/10/200',
+    alternate: 'https://picsum.photos/id/1/200',
   },
   {
     id: '2',
-    primary: 'https://i.pinimg.com/736x/7f/5e/de/7f5ede97a8c8c46b05a9e6322e2b12d1.jpg',
-    alternate: 'https://i.pinimg.com/736x/8d/ae/b1/8daeb1e49c043b0df2f9fcbb0f89a1db.jpg',
+    primary: 'https://picsum.photos/id/2/200',
+    alternate: 'https://picsum.photos/id/99/200',
   },
   {
     id: '3',
-    primary: 'https://i.pinimg.com/736x/64/70/d2/6470d283de499f1fc6ce8208f602c0f4.jpg',
-    alternate: 'https://i.pinimg.com/736x/2b/ca/d6/2bcad6870b9318b3085ec6c7643fbb14.jpg',
+    primary: 'https://picsum.photos/id/12/200',
+    alternate: 'https://picsum.photos/id/98/200',
   },
   {
     id: '4',
-    primary: 'https://i.pinimg.com/736x/2e/20/ba/2e20baa1b9fa8f61df7e10acaa4413b5.jpg',
-    alternate: 'https://i.pinimg.com/1200x/af/70/49/af7049655e197c83a0d4089a5332eb01.jpg',
+    primary: 'https://picsum.photos/id/65/200',
+    alternate: 'https://picsum.photos/id/96/200',
   },
   {
     id: '5',
-    primary: 'https://i.pinimg.com/736x/93/19/5e/93195e1a6322841441bc62d8172b3a32.jpg',
-    alternate: 'https://i.pinimg.com/736x/37/8a/51/378a5114a78f37b3798f559fa434b381.jpg',
+    primary: 'https://picsum.photos/id/95/200',
+    alternate: 'https://picsum.photos/id/94/200',
   },
   {
     id: '6',
-    primary: 'https://i.pinimg.com/736x/8d/b5/a2/8db5a20a2b2855c3cf256600fb18470d.jpg',
-    alternate: 'https://i.pinimg.com/736x/dc/28/d0/dc28d0c0fd6d31ca53330c5b66e8d80f.jpg',
+    primary: 'https://picsum.photos/id/93/200',
+    alternate: 'https://picsum.photos/id/92/200',
   },
   {
     id: '7',
-    primary: 'https://i.pinimg.com/736x/bd/66/94/bd6694b312091fa4333aa90e089fb55a.jpg',
-    alternate: 'https://i.pinimg.com/1200x/01/bb/0a/01bb0a8180d67c35e139c02b10f41553.jpg',
+    primary: 'https://picsum.photos/id/91/200',
+    alternate: 'https://picsum.photos/id/90/200',
   },
   {
     id: '8',
-    primary: 'https://i.pinimg.com/736x/e3/aa/17/e3aa175ead3fd9064ce4ef128973fd96.jpg',
-    alternate: 'https://i.pinimg.com/736x/2e/fb/a9/2efba901d4ae5c004c363252a0bb54e6.jpg',
+    primary: 'https://picsum.photos/id/89/200',
+    alternate: 'https://picsum.photos/id/88/200',
   },
   {
     id: '9',
-    primary: 'https://i.pinimg.com/736x/dc/14/91/dc1491d3f5fa357926c873be8036943c.jpg',
-    alternate: 'https://i.pinimg.com/736x/7d/f1/dd/7df1ddb428a923cc01c0825e0db42c7a.jpg',
+    primary: 'https://picsum.photos/id/87/200',
+    alternate: 'https://picsum.photos/id/86/200',
   },
 ];
 
