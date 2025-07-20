96c42381f8f6858a0ef15e88b809d1a6919502fc
dityudhist <105841114122@student.unismuh.ac.id>
2025-07-20 15:38:10 +0800

    tugas4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,18 +10,18 @@ export default function RootLayout() {
 
   const [loaded, error] = useFonts({
     //font variable
-    "merriweather": require("../assets/fonts/Merriweather-VariableFont_opsz,wdth,wght.ttf"),
-    "oswald": require("../assets/fonts/Oswald-VariableFont_wght.ttf"),
-    "raleway": require("../assets/fonts/Raleway-VariableFont_wght.ttf"),
-    "roboto": require("../assets/fonts/RobotoMono-VariableFont_wght.ttf"),
-    "tiktoksans": require("../assets/fonts/TikTokSans-VariableFont_opsz,slnt,wdth,wght.ttf"),
+    "merriweather": require("../assets/fonts/variable/Merriweather-VariableFont_opsz,wdth,wght.ttf"),
+    "oswald": require("../assets/fonts/variable/Oswald-VariableFont_wght.ttf"),
+    "raleway": require("../assets/fonts/variable/Raleway-VariableFont_wght.ttf"),
+    "roboto": require("../assets/fonts/variable/RobotoMono-VariableFont_wght.ttf"),
+    "tiktoksans": require("../assets/fonts/variable/TikTokSans-VariableFont_opsz,slnt,wdth,wght.ttf"),
 
     //font statis
-    "ubuntu": require("../assets/fonts/Ubuntu-Italic.ttf"),
-    "barlowCondensed": require("../assets/fonts/BarlowCondensed-Regular.ttf"),
-    "ptsans": require("../assets/fonts/PTSans-Regular.ttf"),
-    "rubik": require("../assets/fonts/Rubik-Regular.ttf"),
-    "tilitium": require("../assets/fonts/TitilliumWeb-Regular.ttf")
+    "ubuntu": require("../assets/fonts/static/Ubuntu-Italic.ttf"),
+    "barlowCondensed": require("../assets/fonts/static/BarlowCondensed-Regular.ttf"),
+    "ptsans": require("../assets/fonts/static/PTSans-Regular.ttf"),
+    "rubik": require("../assets/fonts/static/Rubik-Regular.ttf"),
+    "tilitium": require("../assets/fonts/static/TitilliumWeb-Regular.ttf")
   })
 
   useEffect(()=>{
