225eb2501fcc95fdcf42bb1e307b029fa5c8a6a8
dityudhist <105841114122@student.unismuh.ac.id>
2025-07-20 14:59:05 +0800

    tugas4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,15 +9,18 @@ Splashscreen.preventAutoHideAsync();
 export default function RootLayout() {
 
   const [loaded, error] = useFonts({
-    "ubuntu": require("../assets/fonts/Ubuntu-Italic.ttf"),
+    //font variable
     "merriweather": require("../assets/fonts/Merriweather-VariableFont_opsz,wdth,wght.ttf"),
-    "barlowCondensed": require("../assets/fonts/BarlowCondensed-Regular.ttf"),
     "oswald": require("../assets/fonts/Oswald-VariableFont_wght.ttf"),
-    "ptsans": require("../assets/fonts/PTSans-Regular.ttf"),
     "raleway": require("../assets/fonts/Raleway-VariableFont_wght.ttf"),
     "roboto": require("../assets/fonts/RobotoMono-VariableFont_wght.ttf"),
-    "rubik": require("../assets/fonts/Rubik-Regular.ttf"),
     "tiktoksans": require("../assets/fonts/TikTokSans-VariableFont_opsz,slnt,wdth,wght.ttf"),
+
+    //font statis
+    "ubuntu": require("../assets/fonts/Ubuntu-Italic.ttf"),
+    "barlowCondensed": require("../assets/fonts/BarlowCondensed-Regular.ttf"),
+    "ptsans": require("../assets/fonts/PTSans-Regular.ttf"),
+    "rubik": require("../assets/fonts/Rubik-Regular.ttf"),
     "tilitium": require("../assets/fonts/TitilliumWeb-Regular.ttf")
   })
 
